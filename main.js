5aecfadfcd984f38d7fc91a9b448424aa926e45a
Bruna de Moura <brunninhademoura@gmail.com>
2021-10-04 15:38:31 -0300

    menu e ações do menu

---
+++ main.js
@@ -0,0 +1,32 @@
+//abrir e fechar menu: hamburguer e x
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+    element.addEventListener('click', function() {
+        nav.classList.toggle('show')
+    })
+}
+
+//fechar menu quando clicar em um item para
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+    link.addEventListener('click', function() {
+        nav.classList.remove('show')
+    })
+}
+
+//colocar sobra quando der scroll
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function() {
+    if(window.scrollY >= navHeight) {
+        header.classList.add('scroll')
+    } else {
+        header.classList.remove('scroll')
+    }
+})
+
+//DOM - document object model 
