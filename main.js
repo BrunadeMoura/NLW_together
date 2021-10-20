6fe751430c8371f24722bbba47f0b74a31c9c2ea
Bruna de Moura <brunninhademoura@gmail.com>
2021-10-20 18:26:16 -0300

    seção depoimentos e contato, swiper e scrollreveal

--- main.js
+++ main.js
@@ -1,32 +1,57 @@
 //abrir e fechar menu: hamburguer e x
-const nav = document.querySelector('#header nav')
-const toggle = document.querySelectorAll('nav .toggle')
+const nav = document.querySelector("#header nav");
+const toggle = document.querySelectorAll("nav .toggle");
 
 for (const element of toggle) {
-    element.addEventListener('click', function() {
-        nav.classList.toggle('show')
-    })
+  element.addEventListener("click", function () {
+    nav.classList.toggle("show");
+  });
 }
 
 //fechar menu quando clicar em um item para
-const links = document.querySelectorAll('nav ul li a')
+const links = document.querySelectorAll("nav ul li a");
 
 for (const link of links) {
-    link.addEventListener('click', function() {
-        nav.classList.remove('show')
-    })
+  link.addEventListener("click", function () {
+    nav.classList.remove("show");
+  });
 }
 
 //colocar sobra quando der scroll
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+const header = document.querySelector("#header");
+const navHeight = header.offsetHeight;
 
-window.addEventListener('scroll', function() {
-    if(window.scrollY >= navHeight) {
-        header.classList.add('scroll')
-    } else {
-        header.classList.remove('scroll')
-    }
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= navHeight) {
+    header.classList.add("scroll");
+  } else {
+    header.classList.remove("scroll");
+  }
+});
+
+//testimonials carousel slider swiper
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 1,
+
+  //pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
+
+// scrollreveal: mostrar elementos quando der scroll na página
+
+const scrollReveal = ScrollReveal ({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
 })
 
-//DOM - document object model 
+scrollReveal.reveal(
+    `#home .text, #home .image, #about .image, #about .text, #services .card, #testimonials .testimonials, #contact .links
+    `, { interval: 100 })
+
+//DOM - document object model
