634e023fb95a6223939844242fee77a28b9e683d
Bruna de Moura <brunninhademoura@gmail.com>
2021-11-11 17:24:36 -0300

    responsivo para dispositivos > 1200px

--- main.js
+++ main.js
@@ -18,9 +18,11 @@ for (const link of links) {
 }
 
 //colocar sombra quando der scroll
+ const header = document.querySelector("#header");
+  const navHeight = header.offsetHeight
+  
 function changeHeaderWhenScroll () {
-  const header = document.querySelector("#header");
-  const navHeight = header.offsetHeight;
+ 
 
   if (window.scrollY >= navHeight) {
     header.classList.add("scroll");
@@ -39,6 +41,12 @@ const swiper = new Swiper(".swiper", {
   },
   mousewheel: true,
   keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 });
 
 // scrollreveal: mostrar elementos quando der scroll na página
@@ -55,8 +63,10 @@ scrollReveal.reveal(
     `, { interval: 100 })
 
 //back to top
-function backToTop() {
 const backToTopButton = document.querySelector('.back-to-top')
+
+function backToTop() {
+
 
   if(window.scrollY >= 560) {
     backToTopButton.classList.add('show')
@@ -65,10 +75,37 @@ const backToTopButton = document.querySelector('.back-to-top')
   }
 }
 
+//menu ativo conforme seção
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if(checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+        
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
 //when scroll
 window.addEventListener("scroll", function () {
   changeHeaderWhenScroll ()
   backToTop()
+  activateMenuAtCurrentSection()
 });
 
 //DOM - document object model
